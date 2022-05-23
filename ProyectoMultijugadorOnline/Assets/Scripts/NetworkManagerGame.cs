9a4bf6c28546ff40e510a7bcd09cc5d3f72c7e2b
AndruxKilller7 <cristianandruortegavargas@hotmail.com>
2022-05-23 12:54:26 -0400

    AñadiendoMirror

---
+++ ProyectoMultijugadorOnline/Assets/Scripts/NetworkManagerGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class NetworkManagerGame : NetworkManager
+{
+    public Transform pivotPlayer1;
+    public Transform pivotPlayer2;
+
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
